80d4f2b66824730cb206bc87eabe6fe553150186
傻逼 <136309672+Alan0720o@users.noreply.github.com>
2023-06-14 21:17:44 +0800

    Update paimeng.sh

--- paimeng.sh
+++ paimeng.sh
@@ -9,13 +9,13 @@ apt install pipx  # 安装pipx
 pipx install nb-cli  # 安装nonebot脚手架
 
 nb self install nb-cli-plugin-littlepaimon  # 安装小派蒙脚手架插件
-
+；
 nb paimon create  # 创建小派蒙项目
-
+；
 cd LittlePaimon  # LittlePaimon为你刚刚填写的项目目录名
-
+；
 nb paimon res  # 给出的两次选择均保持默认(回车两次)
-
+；
 nb paimon run playwright install chromium --with-deps  # 安装playwright相关依赖
 
 
