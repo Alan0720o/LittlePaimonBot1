7fd1e53a0336abb245637a5b237c346d5596f195
傻逼 <136309672+Alan0720o@users.noreply.github.com>
2023-06-14 20:53:54 +0800

    Update paimeng.sh

--- paimeng.sh
+++ paimeng.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-echo"如果遇到代码不跑的情况就按回车 yes or no 的选项直接y  2023-6更新"
 sudo -i  # 获取root权限，如果本来就以root权限登录，那可以跳过
+yes | apt update && apt upgrade  # 更新系统
 
-# 更新软件包并升级系统
-apt update -y && apt upgrade -y
+apt install pipx  # 安装pipx
 
-# 安装 pipx
-apt install -y python3-pip
-pip install --user pipx
-export PATH="$HOME/.local/bin:$PATH"
+pipx install nb-cli  # 安装nonebot脚手架
 
-# 设置 pipx 环境变量
-pipx ensurepath
+nb self install nb-cli-plugin-littlepaimon  # 安装小派蒙脚手架插件
 
-# 安装 nb-cli
-pipx install nb-cli
+nb paimon create  # 创建小派蒙项目
 
-# 安装 nonebot 脚手架
-nb self install nb-cli-plugin-littlepaimon
-#帮你擦下屏幕
-clear
+pipx install nb-cli  # 安装nonebot脚手架
 
-# 创建项目并设置相关配置
-nb paimon create&&cd LittlePaimon&&nb paimon res&&nb paimon run playwright install chromium --with-deps
+nb self install nb-cli-plugin-littlepaimon  # 安装小派蒙脚手架插件
+
+nb paimon create  # 创建小派蒙项目
+
+cd LittlePaimon  # LittlePaimon为你刚刚填写的项目目录名
+
+nb paimon res  # 给出的两次选择均保持默认(回车两次)
+
+nb paimon run playwright install chromium --with-deps  # 安装playwright相关依赖
+
+
+
