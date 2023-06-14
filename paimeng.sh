72a8ba5bc6f6f542a1d6656e9b809f9470d7bae0
傻逼 <136309672+Alan0720o@users.noreply.github.com>
2023-06-14 20:44:25 +0800

    Update paimeng.sh

--- paimeng.sh
+++ paimeng.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+echo"如果遇到代码不跑的情况就按回车 yes or no 的选项直接y  2023-6更新"
 sudo -i  # 获取root权限，如果本来就以root权限登录，那可以跳过
-yes | apt update && apt upgrade  # 更新系统
-yes | apt install pipx  # 安装pipx
-yes | pipx install nb-cli  # 安装nonebot脚手架
-yes | nb self install nb-cli-plugin-littlepaimon  # 安装小派蒙脚手架插件
-nb paimon create&&cd LittlePaimon&&nb paimon res&&nb paimon run playwright install chromium --with-deps  # 创建小派蒙项目  # 创建小派蒙项目
-yes | pipx install nb-cli  # 安装nonebot脚手架
-yes | nb self install nb-cli-plugin-littlepaimon  # 安装小派蒙脚手架插件
-cd LittlePaimon  # LittlePaimon为你刚刚填写的项目目录名
-yes | nb paimon res  # 给出的两次选择均保持默认(回车两次)
-yes | nb paimon run playwright install chromium --with-deps  # 安装playwright相关依赖
 
-    此后，你只需要在派蒙的目录下使用命令nb paimon run即可启动小派蒙Bot，并根据你选择的go-cqhttp方式，启动并登录机器人账号。"
+# 更新软件包并升级系统
+apt update -y && apt upgrade -y
+
+# 安装 pipx
+apt install -y python3-pip
+pip install --user pipx
+export PATH="$HOME/.local/bin:$PATH"
+
+# 设置 pipx 环境变量
+pipx ensurepath
+
+# 安装 nb-cli
+pipx install nb-cli
+
+# 安装 nonebot 脚手架
+nb self install nb-cli-plugin-littlepaimon
+#帮你擦下屏幕
+clear
+
+# 创建项目并设置相关配置
+nb paimon create&&cd LittlePaimon&&nb paimon res&&nb paimon run playwright install chromium --with-deps
